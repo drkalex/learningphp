21cb861ac0373e14b44a5b8031e2a03f48ead26d
Esneider Alexander <drkapika25@gmail.com>
2017-03-28 11:19:54 +0200

    Intento de construccion de Índice de ejercicios

--- index.php
+++ index.php
@@ -1,4 +1,45 @@
+<php>
+  <head>
+    <title> Ejercicio 1 </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  </head>
+  <body background="cs_bg_champions.png">
+       <center>  <font color="white"> <h2> Primeros pasos en La Grieta del Invocador </h2>
+Muchos factores entran en juego para triunfar en los Campos de la Justicia. Aquí aprenderás los aspectos básicos del juego, te familiarizarás con las mecánicas básicas gracias a los tutoriales y darás tus primeros pasos en La Grieta del Invocador. </font> </center>
+<hr>
+<hr>
+    <table border="2">
+
+      <tr>  <td> <font color="white"> <h3> Campeones </h3>
+La Liga cuenta con varios arquetipos de campeones, desde malvadas mentes criminales hasta monstruosas y épicas criaturas, pasando por toda clase de personaje que puedas imaginar. Los diferentes campeones cubren diferentes posiciones y estrategias, así que no te desanimes si tu primera elección no se adapta a tu estilo de juego.
+<a href="http://gameinfo.euw.leagueoflegends.com/es/game-info/champions/"> <h4> Campeones </h4> </a> </td>
+            <td>  <font color="white"> <h3>  Cómo controlar a tu campeón </h3>
+Con pequeñas diferencias, los campeones usan un sistema de control tradicional de la ETR.
+Para mover a tu campeón, haz clic derecho sobre el terreno al que quieras moverte.
+Para atacar a un enemigo, haz clic derecho sobre tu objetivo.
+Para lanzar un hechizo, haz clic izquierdo sobre el ícono asociado o usa la tecla de acceso rápido del hechizo (por defecto: Q, W, E o R) y luego haz clic sobre el objetivo.
+Los controles se pueden personalizar a través del menú Opciones. </font> <a href="https://www.google.es/search?q=controles+para+jugar+lol&espv=2&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiil8Gp3fjSAhVIXRoKHf6PCQIQ_AUIBygC&biw=1366&bih=601&dpr=1#imgrc=g0uoX2l9KixpRM:"> <h4> Teclas Para Jugar LoL <h4></a> </td>
+      </tr>
+      <tr>  <td>     <font color="white"> <h3> Carriles </h3>
+Hay tres caminos que conectan tu base con la del enemigo. Estos caminos se llaman carriles y sirven para enfrentarse al equipo enemigo. Para vencer tendrás que avanzar por el carril hasta la base enemiga y destruir el nexo que se encuentra en el centro de su base. </font>  <a href=" https://www.google.es/search?q=controles+para+jugar+lol&espv=2&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiil8Gp3fjSAhVIXRoKHf6PCQIQ_AUIBygC&biw=1366&bih=601&dpr=1#tbm=isch&q=carrilesdelol&*&imgrc=tU4VcrWahmuSzM:" > <h4>Lineas del mapa de LoL </h4> </a> </td>
+            <td>  <font color="white"> <h3> Súbditos </h3>
+Los súbditos son soldados controlados por la IA que se generan en el nexo y avanzan por cada carril hacia la base enemiga, atacando a todo oponente que se encuentren en el camino. Lograr acabar con un súbdito otorgará valioso oro a tu campeón. Los súbditos prefieren enfrentarse unos a otros, pero atacarán a tu campeón si lo encuentran solo en el carril. También cambiarán de objetivo para centrarse en ti si atacas a un campeón aliado cercano. No subestimes el poder de una gran oleada de súbditos, ¡sobre todo al principio de la partida! </font> <a href="https://www.google.es/search?q=controles+para+jugar+lol&espv=2&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiil8Gp3fjSAhVIXRoKHf6PCQIQ_AUIBygC&biw=1366&bih=601&dpr=1#tbm=isch&q=Subditos+de+lol&*&imgdii=GZdMtTaVkFjy7M:&imgrc=5M5aeN6H3b-8jM:"> <h4> Subditos </h4> </a> </td>
+      </tr>
+      <tr> <td>  <font color="white"> <h3> Torretas </h3>
+Las torretas son poderosas estructuras defensivas que defienden cada carril a intervalos regulares, castigando a los enemigos que se pongan a su alcance con mortíferos disparos de energía. Al igual que los súbditos, prefieren atacar a otros objetivos y no a ti, salvo que empieces a combatir contra otro campeón estando a su alcance. Una torreta hostil sólo irá por ti si estás atacando a un campeón enemigo o si eres el único objetivo al alcance. Ten cuidado de no empezar un enfrentamiento bajo una torreta enemiga.</font><a href="https://www.google.es/search?q=controles+para+jugar+lol&espv=2&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiil8Gp3fjSAhVIXRoKHf6PCQIQ_AUIBygC&biw=1366&bih=601&dpr=1#tbm=isch&q=torretas+lol&*&imgrc=5NOpqRFmMTlsJM:"> <h4> Torreta </h4> </a> </td>
+            <td>   <font color="white"> <h3> Inhibidores </h3>
+Unas estructuras importantes, conocidas como inhibidores, se encuentran en el punto donde cada carril llega a la base, a ambos lados del mapa. Destruir un inhibidor crea un poderoso súper súbdito cada vez que aparezca una nueva oleada de súbditos en ese carril. Los súper súbditos son muy resistentes, por lo que son ideales para liderar la carga sobre el nexo enemigo. Los inhibidores reaparecen tras cinco minutos, así que aprovecha tu ventaja durante este tiempo.
+Si se destruyen los tres inhibidores enemigos, serán dos los súper súbditos que se generen en cada carril cada vez que se cree una nueva oleada de súbditos.</font><a href="https://www.google.es/search?q=controles+para+jugar+lol&espv=2&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiil8Gp3fjSAhVIXRoKHf6PCQIQ_AUIBygC&biw=1366&bih=601&dpr=1#tbm=isch&q=inhibidor+lol&*&imgrc=KX03C0qKaOY9GM:"> <h4> Inhibidor </h4> </a> </td>
+      </tr>
+      <tr> <td> <font color="white"> <h3>  El nexo </h3>
+El nexo se encuentra en el centro de cada base. Protegido por dos torretas, el nexo es la estructura que genera oleadas de súbditos en cada carril de forma periódica. Cuando un equipo haya destruido el nexo enemigo, la partida habrá terminado y ese equipo será declarado vencedor. </font> <a href="https://www.google.es/search?q=controles+para+jugar+lol&espv=2&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiil8Gp3fjSAhVIXRoKHf6PCQIQ_AUIBygC&biw=1366&bih=601&dpr=1#tbm=isch&q=nexo+lol&*&imgdii=vIO9tcqcUR6btM:&imgrc=Y6igZCvRT2-mcM:"> <h4> Nexo </h4> </a></td>
+          <td> <font color="white"> <h3>  La jungla </h3>
+Entre los carriles se encuentra la jungla, una zona extensa y llena de monstruos neutrales. Estos monstruos no atacarán la base enemiga, pero derrotarlos otorga oro adicional y mejoras temporales especiales. Un jugador podrá ocupar la posición de campeón de jungla y usar estas zonas como principal fuente de ingresos. Los campeones de jungla suelen moverse entre los carriles y acudir en ayuda de los aliados del carril cuando sea necesario. </font> <a href="https://www.google.es/search?q=controles+para+jugar+lol&espv=2&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiil8Gp3fjSAhVIXRoKHf6PCQIQ_AUIBygC&biw=1366&bih=601&dpr=1#tbm=isch&q=jungla+lol&*&imgrc=l67Ww96fUGfOlM:"> <h4> Jungla </h4> </a></td>
+      </tr>
+    </table>
+  </font>
 <?php
+
 
 echo "Hola!"
 
