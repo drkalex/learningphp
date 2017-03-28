66c1a5702be03fc0c7e2056c2bfaebb4cc1f7de9
Esneider Alexander <drkapika25@gmail.com>
2017-03-28 11:21:26 +0200

    Intento de construccion de Índice de ejercicios2

---
+++ ej001.php
@@ -0,0 +1,8 @@
+<php>
+  <head>
+    <title> Ejercicio 2 </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  </head>
+  <body>
+    <form>
+      Nombre <input type="text" name="nombre">
